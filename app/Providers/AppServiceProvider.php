64bbe2a31fd4b1f83bfc36b4a6a7faf36565d922
KhimPatchi <khimdavin24@gmail.com>
2026-07-03 17:46:36 +0800

    Force HTTPS scheme in AppServiceProvider to fix form action mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production (fixes Mixed Content errors behind Azure proxy)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         // Add no-cache headers for all responses
         Response::macro('noCache', function ($content) {
             return Response::make($content)
